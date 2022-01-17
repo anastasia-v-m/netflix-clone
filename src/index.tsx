5b65109b1b154e47b1f7528c7cfa24ae981298d1
anastasia-v-m <matushevskayaanastasia@gmail.com>
2022-01-18 00:36:31 +0300

    feat: add test react code. If it is OK it will be a base for task#5

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './test';
-
+import Announcements from './test-annonces';
 
 ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<Announcements />, document.getElementById('root'));

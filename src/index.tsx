99d15ab3f8a0777a250644c8250c9c97aa0b3c61
Arxons <vasyrin1312@gmail.com>
2022-01-17 13:35:57 +0300

    feat: recreate repo structure

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './test';
+
+
+ReactDOM.render(<Test />, document.getElementById('root'));
---
+++ src/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div className="test">
+      test
+    </div>
+  );
+}
+
+export default Test;

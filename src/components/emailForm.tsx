0211acb44d8c0bfc6828f2594387e45f6635f1dd
Stanislav Vasyrin <vasyrin1312@gmail.com>
2022-01-25 13:28:17 +0300

    feat: update and merge questionsSpot

--- src/components/emailForm.tsx
+++ src/components/emailForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IProps, IState } from './langSelection';
-import landingStartCardData from "../modules/LandingHeader/landingStartCardData.json";
+import landingStartCardData from '../modules/LandingHeader/landingStartCardData.json';
 import Buttons from './button';
 
 export default class EmailForm extends React.Component<IProps, IState> {
@@ -10,36 +10,39 @@ export default class EmailForm extends React.Component<IProps, IState> {
       value: '',
       formTitle: landingStartCardData.formTitle,
       placeLabel: landingStartCardData.placeLabel,
-      startButton: landingStartCardData.startButton
-      };
+      startButton: landingStartCardData.startButton,
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   render() {
     return (
-        <form className='emailForm'>
-          <h3>{this.state.formTitle}</h3>
-          <div className='emailFormContainer'>
-            <label htmlFor="email">
-            <input 
-              type="email" 
-              className='emailInput' 
-              id="email" value={this.state.value} 
-              onChange={this.handleChange} 
-              placeholder={this.state.placeLabel}/>
+      <form className="emailForm">
+        <h3>{this.state.formTitle}</h3>
+        <div className="emailFormContainer">
+          <label htmlFor="email">
+            <input
+              type="email"
+              className="emailInput"
+              id="email"
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder={this.state.placeLabel}
+            />
           </label>
-          <Buttons 
-            name='start-button' 
-            nameContent='start-button-content' 
-            content={this.state.startButton!} 
-            icon='start-button-arrow'/>
-          </div>          
-        </form>    
-    )
+          <Buttons
+            name="start-button"
+            nameContent="start-button-content"
+            content={this.state.startButton!}
+            icon="start-button-arrow"
+          />
+        </div>
+      </form>
+    );
   }
 }

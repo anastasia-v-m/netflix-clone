7c050dc4a9c8d18916b386e1f43e0844a0db7e7d
divakotova <divakova.dasha@yandex.by>
2022-02-09 13:59:19 +0300

    feat: add landing-start-spot instead landing-header

---
+++ src/modules/LandingStartSpot/LandingStartSpot.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import EmailForm from '../../components/EmailForm';
+import data from './data.json';
+
+import '../Header/header.scss';
+
+export default function LandingStartSpot(): JSX.Element {
+  return (
+    <div className="start-card">
+      <div className="start-card-background">
+        <img className="start-card-img" src={data.imgSrc} alt={data.altDescription} />
+        <div className="img-gradient" />
+      </div>
+      <div className="start-card-content">
+        <h1>{data.title}</h1>
+        <h2>{data.annotation}</h2>
+        <EmailForm />
+      </div>
+    </div>
+  );
+}

fbacfd2d93bb9c8de34c715e408f725f1773a684
anastasia-v-m <matushevskayaanastasia@gmail.com>
2022-02-19 19:37:23 +0300

    fix: add header and footer

--- src/containers/FilmDetailsPage/FilmDetailsPage.tsx
+++ src/containers/FilmDetailsPage/FilmDetailsPage.tsx
@@ -3,6 +3,8 @@ import Button from '../../components/Button';
 import FilmDetailsCreateSpot from './FilmDetailsCreateSpot';
 import FilmDetailsSimilarsSpot from './FilmDetailsSimilarSpot';
 import FilmDetailsTrailersSpot from './FilmDetailsTrailersSpot';
+import Header from '../../modules/Header/Header';
+import Footer from '../../modules/Footer';
 
 import './filmDetailsPage.scss';
 
@@ -85,70 +87,74 @@ export default function FilmDetailsPage(props: { movieID: string }): JSX.Element
   }
 
   return (
-    <main className="film-details-main">
-      <section className="film-details-sect">
-        <div className="film-details-basic-container">
-          <div className="film-details-maininfo">
-            <div className="film-title-info-container">
-              <h1 className="film-title">{gotData.original_title}</h1>
-              <p className="film-tags">
-                  {genres.map((item) => (
-                    <Fragment key={`d-${item.id}`}>
-                      <span className="film-tags__tag-wrapper">{item.name}</span>
-                      <span className="film-tags-delimitor"> | </span>
-                    </Fragment>
-                  ))}
-              </p>
-              <h4 className="film-about">{gotData.overview}</h4>
-              <div className="film-details-people">
-                <p>
-                  <span className="film-details-people__titles">{releaseTitle}</span>
-                  {gotData.release_date}
+    <>
+      <Header type="HEADER_404_PAGE" name="page404-header black-bkg" />
+      <main className="film-details-main">
+        <section className="film-details-sect">
+          <div className="film-details-basic-container">
+            <div className="film-details-maininfo">
+              <div className="film-title-info-container">
+                <h1 className="film-title">{gotData.original_title}</h1>
+                <p className="film-tags">
+                    {genres.map((item) => (
+                      <Fragment key={`d-${item.id}`}>
+                        <span className="film-tags__tag-wrapper">{item.name}</span>
+                        <span className="film-tags-delimitor"> | </span>
+                      </Fragment>
+                    ))}
                 </p>
-                <p>
-                  <span className="film-details-people__titles">{ratingTitle}</span>
-                    {gotData.vote_average}
-                </p>
+                <h4 className="film-about">{gotData.overview}</h4>
+                <div className="film-details-people">
+                  <p>
+                    <span className="film-details-people__titles">{releaseTitle}</span>
+                    {gotData.release_date}
+                  </p>
+                  <p>
+                    <span className="film-details-people__titles">{ratingTitle}</span>
+                      {gotData.vote_average}
+                  </p>
+                </div>
               </div>
             </div>
+            <div className="film-details-poster-container">
+              <div className="film-details-poster" style={bkg} />
+            </div>
           </div>
-          <div className="film-details-poster-container">
-            <div className="film-details-poster" style={bkg} />
+          <div className="film-details-to-reg-container">
+            <svg className="film-details-logo" viewBox="225 0 552 1000">
+              <defs>
+                <radialGradient r="75%" gradientTransform="matrix(.38 0 .5785 1 .02 0)">
+                  <stop offset="60%" stopOpacity=".3" />
+                  <stop offset="90%" stopOpacity=".05" />
+                  <stop offset="100%" stopOpacity="0" />
+                </radialGradient>
+              </defs>
+              <path d="M225 0v1000c60-8 138-14 198-17V0H225" fill="#b1060e" />
+              <path d="M579 0v983c71 3 131 9 198 17V0H579" fill="#b1060e" />
+              <path d="M225 0v200l198 600V557l151 426c76 3 136 9 203 17V800L579 200v240L423 0H225" fill="url(#64dd5834-188f-43c7-9b7e-27cb13c1ebd2-a)" />
+              <path d="M225 0l349 983c76 3 136 9 203 17L423 0H225" fill="#e50914" />
+            </svg>
+            <div className="film-details-to-home-wrapper">
+              <span className="film-details-to-home-title">{toHomeTitle}</span>
+              <Button
+                type="BTN_TYPE_SIMPLE"
+                name="film-details-btn-to-home"
+                href="/"
+                content={toHome}
+              />
+            </div>
           </div>
-        </div>
-        <div className="film-details-to-reg-container">
-          <svg className="film-details-logo" viewBox="225 0 552 1000">
-            <defs>
-              <radialGradient r="75%" gradientTransform="matrix(.38 0 .5785 1 .02 0)">
-                <stop offset="60%" stopOpacity=".3" />
-                <stop offset="90%" stopOpacity=".05" />
-                <stop offset="100%" stopOpacity="0" />
-              </radialGradient>
-            </defs>
-            <path d="M225 0v1000c60-8 138-14 198-17V0H225" fill="#b1060e" />
-            <path d="M579 0v983c71 3 131 9 198 17V0H579" fill="#b1060e" />
-            <path d="M225 0v200l198 600V557l151 426c76 3 136 9 203 17V800L579 200v240L423 0H225" fill="url(#64dd5834-188f-43c7-9b7e-27cb13c1ebd2-a)" />
-            <path d="M225 0l349 983c76 3 136 9 203 17L423 0H225" fill="#e50914" />
-          </svg>
-          <div className="film-details-to-home-wrapper">
-            <span className="film-details-to-home-title">{toHomeTitle}</span>
-            <Button
-              type="BTN_TYPE_SIMPLE"
-              name="film-details-btn-to-home"
-              href="/"
-              content={toHome}
-            />
-          </div>
-        </div>
-      </section>
-      <section className="film-description-sect">
-        <div className="film-description-border" />
-        <div className="film-description-container">{gotData.tagline === '' ? gotData.overview : gotData.tagline}</div>
-        <div className="film-description-border" />
-      </section>
-      <FilmDetailsTrailersSpot filmName={gotData.title} />
-      <FilmDetailsCreateSpot productionCompanies={gotData.production_companies} movieID={movieID} />
-      <FilmDetailsSimilarsSpot movieID={movieID} />
-    </main>
+        </section>
+        <section className="film-description-sect">
+          <div className="film-description-border" />
+          <div className="film-description-container">{gotData.tagline === '' ? gotData.overview : gotData.tagline}</div>
+          <div className="film-description-border" />
+        </section>
+        <FilmDetailsTrailersSpot filmName={gotData.title} />
+        <FilmDetailsCreateSpot productionCompanies={gotData.production_companies} movieID={movieID} />
+        <FilmDetailsSimilarsSpot movieID={movieID} />
+      </main>
+      <Footer />
+    </>
   );
 }
